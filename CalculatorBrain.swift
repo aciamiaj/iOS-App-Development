2c39a2ebdbe134b54b3898868b0cc57f7ceabddf
Jaimaica Daisy Eugenio <jdsnavarro@gmail.com>
2023-02-04 17:51:26 -0500

    Assignment 1

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -1,29 +1,25 @@
-
-
 import Foundation
 
 class CalculatorBrain {
-    
-    func doMath(num1 : Int, num2 : Int, op: String) -> Int {
-        var result = 0
-        switch op {
-            case "+":
-                result = num1 + num2
-                break
-            case "-":
-                result = num1 - num2
-                break
-            case "x":
-                result = num1 * num2
-                break
-            case "÷":
-                result = num1 / num2
-                break
-            default:
-                break
-        }
-            return result
-        
+
+  func doMath(num1: Int, num2: Int, op: String) -> Int {
+    var result = 0
+    switch op {
+    case "+":
+      result = num1 + num2
+      break
+    case "-":
+      result = num1 - num2
+      break
+    case "x":
+      result = num1 * num2
+      break
+    case "÷":
+      result = num1 / num2
+      break
+    default:
+      break
     }
-    
+    return result
+  }
 }
